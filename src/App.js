33f645187f00e076b5761300083afbfc9d8041af
Sanaulla Shaikh <sanaulla@0101digitall.com>
2025-07-03 00:25:55 +0530

    Refactor App component to fetch and display country data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  // countries = [
+  // { name: "Afghanistan", flag: "https://flagcdn.com/w320/af.png", abbr: "af" },
+  // ] ]
+  useEffect(() => {
+    fetch("https://xcountries-backend.azurewebsites.net/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching countries:", error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Country App</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          justifyItems: "center",
+          margin: "20px auto",
+          width: "80%",
+          flexWrap: "wrap",
+          gap: "20px",
+        }}
+      >
+        {countries.length > 0 ? (
+          countries.map((country) => (
+            <div
+              key={country.abbr}
+              style={{
+                border: "1px solid #ccc",
+                padding: "10px",
+                borderRadius: "5px",
+                textAlign: "center",
+                width: "120px",
+                marginBottom: "auto",
+              }}
+            >
+              <img
+                src={country.flag}
+                alt={"Flag of " + country.name}
+                style={{ width: "100px", height: "60px" }}
+              />
+              <p>{country.name}</p>
+            </div>
+          ))
+        ) : (
+          <p>Loading countries...</p>
+        )}
+      </div>
     </div>
   );
 }
